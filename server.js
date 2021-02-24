f5204b2e3a36044eb367e92370e827dc62633e29
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-24 12:20:11 -0500

    commented out routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
