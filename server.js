de7229b99896cca5c0ffa9241d33204919485c46
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-24 13:10:40 -0500

    updated server file

--- server.js
+++ server.js
@@ -25,17 +25,17 @@ mongoose
     console.log("Error connecting to MongoDB", err);
   });
 
-// app.get("/api/config", (req, res) => {
-//   res.json({
-//     success: true,
-//   });
-// });
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
 
 // app.use(routes);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
