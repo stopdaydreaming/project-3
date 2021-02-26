c0f82c10905e48c6ed666fa458b6712fbd590fd5
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-25 20:33:35 -0500

    setup create user page

---
+++ client/src/pages/CreateUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+const CreateUser = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Create User Page</h1>
+        </div>
+    );
+};
+
+export default CreateUser;
