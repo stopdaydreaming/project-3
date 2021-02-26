f5c8c280956392d3622859a23635943f81927ab6
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-26 13:54:38 -0500

    updated profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from '../components/Profile/Profile';
+
+const Profile = () => {
+    return (
+        <div>
+            <Profile />
+        </div>
+    );
+};
+
+export default Profile;
