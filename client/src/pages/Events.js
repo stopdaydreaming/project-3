07d8d04fb47adcf43435cdf445b23c70846dc7a6
dlhardi <derricklhardison@gmail.com>
2021-02-25 22:01:19 -0500

    Created Events page

---
+++ client/src/pages/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Events = () => {
+    return (
+        <div>
+            <h1>This is the Events Page</h1>
+        </div>
+    );
+};
+
+export default Events;
