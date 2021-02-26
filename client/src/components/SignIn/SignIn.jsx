0691bed1c5359d6687953c184961e4deb9d42c70
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-25 19:30:21 -0500

    removed password field

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -33,15 +33,6 @@ class SignIn extends Component {
               </div>
             </div>
 
-            <div class="space-y-1">
-              {/* <label for="password" class="block text-sm font-medium text-gray-700">
-                New to Animeet? Create an account
-              </label> */}
-              <div class="mt-1">
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
-              </div>
-            </div>
-
             <div class="flex items-center justify-between">
               <div class="flex items-center">
                 <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
