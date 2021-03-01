3424da2761f2a5477cfdfc206809b77b1776d3ed
dlhardi <derricklhardison@gmail.com>
2021-03-01 13:00:02 -0500

    added routes for events

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
-const characterRoutes = require("./characters")
-const userRoutes = require ("./users")
+const characterRoutes = require("./characters");
+const userRoutes = require("./users");
+const eventRoutes = require("./events")
 
 // characters routes
-
 
 router.use("/characters", characterRoutes);
 
 router.use("/users", userRoutes);
+
+router.use("/events", eventRoutes);
 
 module.exports = router;
