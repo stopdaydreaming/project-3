34d901eb9e56779656c7fb55925d65991cc4bffd
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-01 19:45:27 -0500

    changed to class to className

--- client/src/components/Radio/Radio.jsx
+++ client/src/components/Radio/Radio.jsx
@@ -4,48 +4,48 @@ class Radio extends Component {
     render() {
         return (
           <fieldset>
-            <legend class="sr-only">
+            <legend className="sr-only">
               Privacy setting
             </legend>
           
-            <div class="bg-white rounded-md -space-y-px">
-              <div class="relative border rounded-tl-md rounded-tr-md p-4 flex">
-                <div class="flex items-center h-5">
-                  <input id="settings-option-0" name="privacy_setting" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300" checked/>
+            <div className="bg-white rounded-md -space-y-px">
+              <div className="relative border rounded-tl-md rounded-tr-md p-4 flex">
+                <div className="flex items-center h-5">
+                  <input id="settings-option-0" name="privacy_setting" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300" checked/>
                 </div>
-                <label htmlFor="settings-option-0" class="ml-3 flex flex-col cursor-pointer">
-                  <span class="block text-sm font-medium">
+                <label htmlFor="settings-option-0" className="ml-3 flex flex-col cursor-pointer">
+                  <span className="block text-sm font-medium">
                     Public access
                   </span>
-                  <span class="block text-sm">
+                  <span className="block text-sm">
                     This project would be available to anyone who has the link
                   </span>
                 </label>
               </div>
           
-              <div class="relative border border-gray-200 p-4 flex">
-                <div class="flex items-center h-5">
-                  <input id="settings-option-1" name="privacy_setting" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300"/>
+              <div className="relative border border-gray-200 p-4 flex">
+                <div className="flex items-center h-5">
+                  <input id="settings-option-1" name="privacy_setting" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300"/>
                 </div>
-                <label htmlFor="settings-option-1" class="ml-3 flex flex-col cursor-pointer">
-                  <span class="block text-sm font-medium">
+                <label htmlFor="settings-option-1" className="ml-3 flex flex-col cursor-pointer">
+                  <span className="block text-sm font-medium">
                     Private to Project Members
                   </span>
-                  <span class="block text-sm">
+                  <span className="block text-sm">
                     Only members of this project would be able to access
                   </span>
                 </label>
               </div>
           
-              <div class="relative border border-gray-200 rounded-bl-md rounded-br-md p-4 flex">
-                <div class="flex items-center h-5">
-                  <input id="settings-option-2" name="privacy_setting" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300"/>
+              <div className="relative border border-gray-200 rounded-bl-md rounded-br-md p-4 flex">
+                <div className="flex items-center h-5">
+                  <input id="settings-option-2" name="privacy_setting" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 cursor-pointer border-gray-300"/>
                 </div>
-                <label htmlFor="settings-option-2" class="ml-3 flex flex-col cursor-pointer">
-                  <span class="block text-sm font-medium">
+                <label htmlFor="settings-option-2" className="ml-3 flex flex-col cursor-pointer">
+                  <span className="block text-sm font-medium">
                     Private to you
                   </span>
-                  <span class="block text-sm">
+                  <span className="block text-sm">
                     You are the only one able to access this project
                   </span>
                 </label>
