bf896ee51e09603d9e852d32bbe87840b63b32f7
dlhardi <derricklhardison@gmail.com>
2021-03-02 05:10:27 -0500

    Created login compnent for Google button

---
+++ client/src/components/Login/HookLogin.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useGoogleLogin } from "react-google-login";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
+
+const clientId =
+  "290641540556-ks696c7llt77ibqkmli3l416nbk15ima.apps.googleusercontent.com";
+
+   const onSignIn =(googleUser)  => {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+   }
+       
+
+ // This is null if the 'email' scope is not present.
+  
+
+function HookLogin() {
+
+    const history = useHistory();
+
+  const onSuccess = (res) => {
+      
+    console.log("Login Success: currentUser:", res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name}.`
+    );
+    //history.push("/profile");
+  };
+
+  const onFailure = (res) => {
+    console.log("Login failed: res:", res);
+    alert(`Failed to login.`);
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    onSignIn,
+    clientId,
+    isSignedIn: true,
+    accessType: "offline",
+    // responseType: 'code',
+    // prompt: 'consent',
+  });
+
+  return (
+    <div>
+      <button onClick={signIn} className="button">
+        <img src="https://blog.hubspot.com/hubfs/image8-2.jpg" alt="google login" className="icon"></img>
+
+        <span className="buttonText">Sign in with Google</span>
+      </button>
+    </div>
+  );
+}
+
+export default HookLogin;
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import {GoogleLogin} from "react-google-login"
+import { GoogleLogout } from "react-google-login";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
+
+
+class Login extends Component {
+     //history = useHistory();
+
+     responseGoogle = (response) => {
+     console.log(response);
+     console.log(response.profileObj);
+    alert(`Welcome ${response.profileObj.name}`)
+    //history.push("/profile");
+   };
+
+   successMessage =(response) => {
+       alert("Congratulations!")
+   }
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+        // render={renderProps => (
+        //     <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Button</button>
+        // )}
+          clientId="290641540556-ks696c7llt77ibqkmli3l416nbk15ima.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+    );
+  }
+}
+
+export default Login;
