765f83034a827ce178dba78bc072251b1243130a
dlhardi <derricklhardison@gmail.com>
2021-03-02 00:46:57 -0500

    Added button to characters

--- client/src/pages/CharacterSelect.js
+++ client/src/pages/CharacterSelect.js
@@ -52,6 +52,13 @@ const CharacterSelect = () => {
                             <p class="text-indigo-600">{character.animeography[0].name}</p>
                           </div>
                         </div>
+                        
+                        <button
+                  type="submit"
+                  class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Select
+                </button>
                       </div>
                     </li>
                   );
