a02fad6e5e2239191bcf98923be878290379c333
dlhardi <derricklhardison@gmail.com>
2021-03-02 12:21:07 -0500

    removed profileImage from schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-  profileImage: { type: String, },
+  //profileImage: { type: String, },
   mainCharacter: {type: Schema.Types.ObjectId, ref: "Character"},
   city: { type: String, },
   state: { type: String }
