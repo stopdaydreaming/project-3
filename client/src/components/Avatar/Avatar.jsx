52b6518607082edf389fba9ece4df9bc03d6b945
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-01 19:39:49 -0500

    changed to class to className

--- client/src/components/Avatar/Avatar.jsx
+++ client/src/components/Avatar/Avatar.jsx
@@ -4,7 +4,7 @@ class Avatar extends Component {
     render() {
         return (
             <div>
-                <img class="inline-block h-14 w-14 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=uPE37qrihf&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                <img className="inline-block h-14 w-14 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=uPE37qrihf&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
             </div>
         );
     }
