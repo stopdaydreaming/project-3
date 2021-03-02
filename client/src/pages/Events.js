dfc1fd5f594aedd3848aa860ef39d828d325fe19
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 16:04:44 -0500

    removed unused vars

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -161,7 +161,7 @@ const Events = () => {
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                           <a
-                            href="#"
+                            href="/#"
                             className="text-indigo-600 hover:text-indigo-900"
                           >
                             Attend
