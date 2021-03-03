4c2a221899d5758ecc83371bc0660ef806b4c5f0
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-03 11:15:52 -0500

    removed console log

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 const Footer = () => {
   const { pathname } = useLocation();
   useEffect(() => {
-    console.log(pathname);
+    // console.log(pathname);
   }, [pathname]);
   return (
     <>
